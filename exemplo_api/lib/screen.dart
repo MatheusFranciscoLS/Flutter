5f29b62e9a8f25445a61c9afdbaef459751a7a69
MatheusFranciscoLS <matheusfran.ls@gmail.com>
2024-05-13 21:58:04 -0300

    13/05/24

--- exemplo_api/lib/screen.dart
+++ exemplo_api/lib/screen.dart
@@ -1,44 +1,36 @@
 import 'package:flutter/material.dart';
-
 import 'service.dart';
 
 class WeatherScreen extends StatefulWidget {
-  const WeatherScreen({super.key});
+  const WeatherScreen({Key? key}) : super(key: key);
 
   @override
   State<WeatherScreen> createState() => _WeatherScreenState();
 }
 
 class _WeatherScreenState extends State<WeatherScreen> {
-  // Instância do serviço WeatherService para obter os dados de previsão do tempo.
   final WeatherService _weatherService = WeatherService(
-    apiKey:
-        '681126f28e7d6fa3a7cfe0da0671e599', // Chave de API para acesso à API de previsão do tempo.
-    baseUrl:
-        'https://api.openweathermap.org/data/2.5', // URL base da API de previsão do tempo.
+    apiKey: 'b9ebe666087f299f5e2aad3a03d093b6',
+    baseUrl: 'https://api.openweathermap.org/data/2.5',
   );
 
-  // Mapa que armazenará os dados de previsão do tempo.
-  late Map<String, dynamic> _weatherData;
+  final TextEditingController _cityController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+  late Map<String, dynamic> _weatherData = {};
 
   @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _weatherData = new Map<String, dynamic>();
+  void dispose() {
+    _cityController.dispose();
+    super.dispose();
   }
 
-  // Método assíncrono para buscar os dados de previsão do tempo para uma cidade específica.
   Future<void> _fetchWeatherData(String city) async {
     try {
-      // Obtém os dados de previsão do tempo para a cidade especificada.
       final weatherData = await _weatherService.getWeather(city);
-      // Atualiza o estado do widget com os novos dados de previsão do tempo.
       setState(() {
         _weatherData = weatherData;
       });
     } catch (e) {
-      // Em caso de erro ao buscar os dados de previsão do tempo, exibe uma mensagem de erro no console.
       print(e);
     }
   }
@@ -50,28 +42,53 @@ class _WeatherScreenState extends State<WeatherScreen> {
         title: Text("Exemplo Weather-API"),
       ),
       body: Padding(
-          padding: const EdgeInsets.all(12),
-          child: FutureBuilder(
-              future: _fetchWeatherData("Limeira"),
-              builder: (context, snapshot) {
-                if (_weatherData.isEmpty) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                } else {
-                  return Center(
-                      child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                        Text(
-                            'City: ${_weatherData['name']}'), // Exibe o nome da cidade.
-                        Text(
-                            'Temperature: ${_weatherData['main']['temp'] - 273} °C'), // Exibe a temperatura em graus Celsius.
-                        Text(
-                            'Description: ${_weatherData['weather'][0]['description']}'), // Exibe a descrição do clima.
-                      ]));
-                }
-              })),
+        padding: const EdgeInsets.all(12),
+        child: Center(
+          child: Form(
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: _cityController,
+                  decoration: InputDecoration(
+                    labelText: "Digite o nome da cidade",
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value!.trim().isEmpty) {
+                      return "Insira o nome da cidade";
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    _fetchWeatherData(_cityController.text);
+                  },
+                  child: Text("Buscar"),
+                ),
+                SizedBox(height: 20),
+                if (_weatherData.isNotEmpty)
+                  Column(
+                    children: [
+                      Text(
+                        'City: ${_weatherData['name']}',
+                      ),
+                      Text(
+                        'Temperature: ${(_weatherData['main']['temp'] - 273).toStringAsFixed(2)} °C',
+                      ),
+                      Text(
+                        'Description: ${_weatherData['weather'][0]['description']}',
+                      ),
+                    ],
+                  ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
