017106eafeb47747bc50022bbd293e697a3fc9ab
MatheusFranciscoLS <matheusfran.ls@gmail.com>
2024-06-04 22:00:35 -0300

    04/06/24

--- exemplo_firebase/lib/services/auth_service.dart
+++ exemplo_firebase/lib/services/auth_service.dart
@@ -9,11 +9,31 @@ class AuthService {
     try {
       UserCredential userCredential = await _auth.signInWithEmailAndPassword(
           email: email, password: password);
-      User user = userCredential.user!;
-      return user;
+      return userCredential.user;
     } catch (e) {
-      print(e);
+      print(e.toString());
       return null;
+    }
+  }
+
+  //metodo register user
+  Future<User?> registerUsuario(String email, String password) async {
+    try {
+      UserCredential userCredential = await _auth
+          .createUserWithEmailAndPassword(email: email, password: password);
+      return userCredential.user;
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  //logout user
+  Future<void> logoutUsuario() async {
+    try {
+      await _auth.signOut();
+    } catch (e) {
+      print(e.toString());
     }
   }
 }
