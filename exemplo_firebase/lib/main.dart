70da410e9713b5af961abdc00b963cd0f4eee426
MatheusFranciscoLS <matheusfran.ls@gmail.com>
2024-06-06 19:07:33 -0300

    05/06/24

--- exemplo_firebase/lib/main.dart
+++ exemplo_firebase/lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:exemplo_firebase/firebase_options.dart';
+import 'package:exemplo_firebase/screens/home_screen.dart';
 import 'package:exemplo_firebase/screens/register_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
-import 'screens/home_screen.dart';
 import 'screens/login_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
@@ -21,8 +22,9 @@ class MainApp extends StatelessWidget {
       home: const HomeScreen(),
       debugShowCheckedModeBanner: false,
       routes: {
-        '/login': (context) => const LoginScreen(),
-        '/register': (context) => const RegisterScreen(),
+        '/home':(context)=> const HomeScreen(),
+        '/login':(context) => const LoginScreen(),
+        '/register':(context) => const RegisterScreen(),
       },
     );
   }
