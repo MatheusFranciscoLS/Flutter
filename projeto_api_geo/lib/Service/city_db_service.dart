b51dcd3ae976064580b6bcff2cb1511181fa2bf4
MatheusFranciscoLS <matheusfran.ls@gmail.com>
2024-06-19 19:51:56 -0300

    19/06/24

--- projeto_api_geo/lib/Service/city_db_service.dart
+++ projeto_api_geo/lib/Service/city_db_service.dart
@@ -6,85 +6,69 @@ import '../Model/city_model.dart';
 class CityDataBaseService {
   static const String DB_NOME = 'city.db'; // Nome do banco de dados
   static const String TABLE_NOME = 'cities'; // Nome da tabela
-  static const String CREATE_CONTACTS_TABLE_SCRIPT = // Script SQL para criar a tabela
-      """CREATE TABLE cities(
-        id SERIAL, 
-        cityname TEXT, 
-        historycities BOOLEAN)""";
 
-  Future<Database> _getDatabase() async{
+  static const String CREATE_CONTACTS_TABLE_SCRIPT =
+      """CREATE TABLE cities(
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      cityname TEXT,
+      historycities INTEGER)""";
+
+  Future<Database> _getDatabase() async {
     return openDatabase(
       join(await getDatabasesPath(), DB_NOME), // Caminho do banco de dados
       onCreate: (db, version) {
-        return db.execute(
-            CREATE_CONTACTS_TABLE_SCRIPT); // Executa o script de criação da tabela quando o banco é criado
+        return db.execute(CREATE_CONTACTS_TABLE_SCRIPT); // Executa o script de criação da tabela
       },
       version: 1,
     );
   }
 
- Future<List<City>> getAllCities() async {
-  Database db = await _getDatabase();
-  List<Map<String, dynamic>> maps = await db.query(TABLE_NOME);
-  return List.generate(
-    maps.length,
-    (i) {
-      return City(
-        cityName: maps[i]['cityname'],
-        historyCities: maps[i]['historycities'] == 1, // Convertendo de int para bool
-      );
-    },
-  );
-}
+  // Métodos CRUD
 
+  Future<List<City>> getAllCities() async {
+    Database db = await _getDatabase();
+    List<Map<String, dynamic>> maps = await db.query(TABLE_NOME);
+    return List.generate(
+      maps.length,
+      (i) {
+        return City(
+          cityName: maps[i]['cityname'],
+          historyCities: maps[i]['historycities'] == 1, // Convertendo de int para bool
+        );
+      },
+    );
+  }
 
   Future<int> insertCity(City city) async {
     try {
       Database db = await _getDatabase();
-      print("banco");
       return await db.insert(TABLE_NOME, city.toMap());
     } catch (e) {
       print(e);
       return 0;
     }
-      }  
-      //update
-Future<void> updateCity(City city) async {
-  try {
-    Database db = await _getDatabase();
-    int isHistoryInt = city.historyCities ? 1 : 0; // Convertendo bool para int
-    await db.update(
-      TABLE_NOME,
-      {'historycities': isHistoryInt},
-      where: 'cityname = ?',
-      whereArgs: [city.cityName],
-    );
-  } catch (e) {
-    print(e);
   }
-}
 
-Future<void> removeCityFromHistory(String cityName) async {
-  try {
-    Database db = await _getDatabase();
-    await db.delete(
-      TABLE_NOME,
-      where: 'cityname = ?',
-      whereArgs: [cityName],
-    );
-  } catch (e) {
-    print(e);
-  }
-}
-
- 
-
-  Future<void> historyCity(String cityName, bool isHistory) async {
+  Future<void> updateCity(City city) async {
     try {
       Database db = await _getDatabase();
+      int isHistoryInt = city.historyCities ? 1 : 0; // Convertendo bool para int
       await db.update(
         TABLE_NOME,
-        {'historycities': isHistory ? 1 : 0},
+        {'historycities': isHistoryInt},
+        where: 'cityname = ?',
+        whereArgs: [city.cityName],
+      );
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> removeCityFromHistory(String cityName) async {
+    try {
+      Database db = await _getDatabase();
+      await db.delete(
+        TABLE_NOME,
         where: 'cityname = ?',
         whereArgs: [cityName],
       );
@@ -93,4 +77,32 @@ Future<void> removeCityFromHistory(String cityName) async {
     }
   }
 
+  Future<void> historyCity(String cityName, bool isHistory) async {
+    try {
+      Database db = await _getDatabase();
+      await db.update(
+        TABLE_NOME,
+        {'historycities': isHistory ? 1 : 0}, // Convertendo booleano para 0 ou 1
+        where: 'cityname = ?',
+        whereArgs: [cityName],
+      );
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<bool> isCityFavorite(String cityName) async {
+    try {
+      Database db = await _getDatabase();
+      List<Map<String, dynamic>> result = await db.query(
+        TABLE_NOME,
+        where: 'cityname = ? AND historycities = 1',
+        whereArgs: [cityName],
+      );
+      return result.isNotEmpty;
+    } catch (e) {
+      print(e);
+      return false;
+    }
+  }
 }
